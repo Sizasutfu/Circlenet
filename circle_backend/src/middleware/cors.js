49d5b965e8c4041bdcbfc6ffae8ecfaee29bb498
Siza Mndzawe <sutfusutfu@gmail.con>
2026-05-22 11:10:55 +0200

    updated cors

--- circle_backend/src/middleware/cors.js
+++ circle_backend/src/middleware/cors.js
@@ -4,7 +4,8 @@ const ALLOWED_ORIGINS = [
   'http://localhost:5501',
   'http://127.0.0.1:5501',
   'http://localhost:7700',
-  'https://circle-app-cm8qwkxqp-sizasutfus-projects.vercel.app'
+  'https://circle-app-cm8qwkxqp-sizasutfus-projects.vercel.app',
+  'www.circlenet.social'
 ];
 
 function cors(req, res, next) {

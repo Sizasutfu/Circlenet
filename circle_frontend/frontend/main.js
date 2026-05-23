d2a3d1aded8c92a6e575e67de70280bd18e15913
Sizasutfu <132141354+Sizasutfu@users.noreply.github.com>
2026-05-23 18:33:35 +0200

    Fix API URL for production environment

--- circle_frontend/frontend/main.js
+++ circle_frontend/frontend/main.js
@@ -3,7 +3,7 @@
 // or a real domain — no hardcoded IP needed.
 
 const API = window.location.hostname === 'www.circlenet.social'
-  ? 'https://circleappapp-production.up.railway.app'
+  ? 'https://https://api.circlenet.social/'
   : window.location.origin;
 
 let posts = [],
